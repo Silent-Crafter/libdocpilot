090903a881b7ec09a35eefeadbf859327fad7efc
Nikhil Solanke <nikhilsolanke5@gmail.com>
2025-01-07 10:15:13 +0530

    cleanup & refactoring

---
+++ dspyclasses.py
@@ -0,0 +1,56 @@
+import dspy
+
+from llama_index.core import VectorStoreIndex
+from signatures import GenerateSearchQuery, GenerateAnswer
+from dsp.utils.utils import deduplicate
+
+from typing import Union, Optional, List
+
+class LlamaIndexRMClient(dspy.Retrieve):
+    def __init__(self, index: VectorStoreIndex, k: int = 3):
+        super().__init__(k=k)
+        self.retriever = index.as_retriever(similarity_top_k = k)
+
+    def forward(
+            self,
+            query: str = None,
+            k: Optional[int] = None,
+            by_prob: bool = True,
+            with_metadata: bool = False,
+            **kwargs,
+    ) -> Union[List[str], dspy.Prediction, List[dspy.Prediction]]:
+
+        nodes = self.retriever.retrieve(query)
+
+        passages = list(map(
+            lambda x: x.text,
+            nodes
+        ))
+
+        return dspy.Prediction(
+            passages=list(reversed(passages)),
+        )
+
+
+class MultiHopRAG(dspy.Module):
+    def __init__(self, index: VectorStoreIndex, num_passages=3, max_hops=3):
+        super().__init__()
+
+        self.generate_query = [dspy.ChainOfThought(GenerateSearchQuery) for _ in range(max_hops)]
+        self.retrieve = LlamaIndexRMClient(k=num_passages, index=index)
+        self.generate_answer = dspy.ChainOfThought(GenerateAnswer)
+        self.max_hops = max_hops
+
+    def forward(self, question):
+        context = []
+
+        for hop in range(self.max_hops):
+            query_resp = self.generate_query[hop](context=context, question=question)
+            query = query_resp.query
+            print(f"Searching with {query=}")
+            passages = self.retrieve(query).passages
+            context = deduplicate(context + passages)
+
+        prediction = self.generate_answer(context=context, question=question)
+        return dspy.Prediction(context=context, answer=prediction.answer)
+
---
+++ utils.py
@@ -0,0 +1,116 @@
+import psycopg2
+
+from llama_index.core import VectorStoreIndex, Document, StorageContext, SimpleDirectoryReader
+from llama_index.vector_stores.postgres import PGVectorStore
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from parsers import CustomXLSXReader
+from sqlalchemy import make_url
+from typing import List
+
+
+def load_docs(dir: str, **kwargs) -> List[Document]:
+    """
+    Load documents from a directory using SimpleDirectoryReader of LlamaIndex
+    :param dir: the directory to load documents from
+    :param kwargs: additional arguments to pass to SimpleDirectoryReader
+    :return: List of Document
+    """
+    file_extractors = {
+        ".xlsx": CustomXLSXReader(),
+    }
+
+    return SimpleDirectoryReader(
+        dir,
+        file_extractor=file_extractors,
+        **kwargs
+    ).load_data()
+
+
+def init_pg(uri):
+    conn = psycopg2.connect(uri)
+    conn.autocommit = True
+
+    return conn
+
+def get_total_nodes(conn, table, col = "id"):
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT count({col}) FROM {table}")
+    return cursor.fetchone()[0]
+
+
+def get_index_from_store(vector_store, embed_model):
+    return VectorStoreIndex.from_vector_store(
+        vector_store=vector_store,
+        embed_model=HuggingFaceEmbedding(model_name=embed_model),
+        show_progress=True
+    )
+
+
+def reindex_vector_store(documents: List[Document], uri, db, embeddings_table: str, embed_model: str):
+    with psycopg2.connect(uri) as conn:
+        with conn.cursor() as cursor:
+            try:
+                cursor.execute('TRUNCATE TABLE {}'.format(embeddings_table))
+                conn.commit()
+            except psycopg2.errors.UndefinedTable:
+                pass
+
+    storage_context, _ = create_vector_store(uri, db, embeddings_table)
+
+    return embed_documents(documents, embed_model, storage_context)
+
+
+def embed_documents(documents: List[Document], embed_model: str, storage_context: StorageContext, **kwargs) -> VectorStoreIndex:
+    """
+    Generate embeddings and store in the vector store. Will store the embeddings regardless of whether they are already present.
+    :param documents: list of documents
+    :param embed_model:  HuggingFace embedding model
+    :param storage_context:  the StorageContext instance to be used to store the embeddings in a vector store
+    :param kwargs: Additional arguments/options to be passed to VectorStoreIndex.from_documents()
+    :return: the index
+    """
+    model_cache_folder = kwargs.get("model_cache_folder", "./models/")
+    show_progress = kwargs.get("show_progress", True)
+
+    return VectorStoreIndex.from_documents(
+        documents,
+        embed_model=HuggingFaceEmbedding(
+            model_name=embed_model,
+            cache_folder=model_cache_folder,
+            trust_remote_code=True,
+        ),
+        show_progress=show_progress,
+        storage_context=storage_context,
+        **kwargs,
+    )
+
+def create_vector_store(uri, db, table, **kwargs) -> (StorageContext, PGVectorStore):
+    """
+    Create a vector store to store embeddings from a URI and a database and table
+    :param uri: Postgres URI
+    :param db: Postgres database
+    :param table: Postgres table
+    :param kwargs: extra configuration options like embed_dim, etc
+    :return: The vector store and storage context
+    """
+    embed_dim = kwargs.get('embed_dim', 768)
+    hnsw_kwargs = kwargs.get('hnsw_kwargs', {
+        "hnsw_m": 16,
+        "hnsw_ef_construction": 64,
+        "hnsw_ef_search": 40,
+        "hnsw_dist_method": "vector_cosine_ops",
+    })
+
+    url = make_url(uri)
+    vector_store = PGVectorStore.from_params(
+        database=db,
+        host=url.host,
+        password=url.password,
+        port=url.port,
+        user=url.username,
+        table_name=table.replace("data_", ""),
+        embed_dim=embed_dim,
+        hnsw_kwargs=hnsw_kwargs,
+    )
+
+    return StorageContext.from_defaults(vector_store=vector_store), vector_store
