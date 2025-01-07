92c02a7ff6b99ebf438a3f652cb97a9c0ac853dd
Nikhil Solanke <nikhilsolanke5@gmail.com>
2025-01-07 10:15:13 +0530

    cleanup & refactoring

--- main.py
+++ main.py
@@ -1,140 +1,72 @@
-import psycopg2
 import textwrap
+import dspy
 
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, StorageContext
-from llama_index.embeddings.ollama import OllamaEmbedding 
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.vector_stores.postgres import PGVectorStore
-from llama_index.llms.ollama import Ollama
-from sqlalchemy import make_url
+from llama_index.core import SimpleDirectoryReader
+
+from parsers import CustomXLSXReader, CustomPDFReader
+from dspyclasses import LlamaIndexRMClient, MultiHopRAG
+from utils import reindex_vector_store, create_vector_store
+
+import pathlib
 
 PG_CONNECTION_URI = "postgresql://postgres:postgres@localhost:5432"
 PG_DB_NAME = "postgres"
 
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
+def m_main():
+    docs = SimpleDirectoryReader("data", file_extractor={".xlsx": CustomXLSXReader()}).load_data()
 
-def init_pg(uri):
-    conn = psycopg2.connect(uri)
-    conn.autocommit = True
+    index = reindex_vector_store(docs, PG_CONNECTION_URI, PG_DB_NAME, "data_items", "ibm-granite/granite-embedding-278m-multilingual")
 
-    return conn
+    multi_hop = MultiHopRAG(index=index, num_passages=10, max_hops=1)
 
-
-def get_total_nodes(conn, table, col = "id"):
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT count({col}) FROM {table}")
-    return cursor.fetchone()[0]
-
-
-def create_vector_store(uri, db, table, **kwargs):
-    
-    embed_dim = kwargs.get('embed_dim', 768)
-    hnsw_kwargs = kwargs.get('hnsw_kwargs', {
-        "hnsw_m": 16,
-        "hnsw_ef_construction": 64,
-        "hnsw_ef_search": 40,
-        "hnsw_dist_method": "vector_cosine_ops",
-    })
-
-    url = make_url(PG_CONNECTION_URI)
-    vector_store = PGVectorStore.from_params(
-        database=db,
-        host=url.host,
-        password=url.password,
-        port=url.port,
-        user=url.username,
-        table_name=table,
-        embed_dim=embed_dim,
-        hnsw_kwargs=hnsw_kwargs,
+    chatbot = dspy.LM(
+        model="ollama/granite-3.1-8b-instruct",
+        system_prompt="Strictly follow the given instructions and adhere to the given format",
+        base_url="http://192.168.0.124:11434/",
     )
 
-    return vector_store, StorageContext.from_defaults(vector_store=vector_store)
+    dspy.settings.configure(lm=chatbot, rm=LlamaIndexRMClient)
 
+    while True:
+        out = multi_hop(input(">> ").strip())
+        print(out.answer)
+        choice = input("History ([Y]/N)? ").strip().lower()
+        if choice == 'n': continue
+        print(chatbot.inspect_history())
+        print()
 
-def load_documents(doc_dir, **kwargs):
-    return SimpleDirectoryReader(doc_dir, **kwargs).load_data()
+def s_main():
+    import os
+    files = os.listdir("data")
 
+    for index, file in enumerate(files):
+        print(f"{index+1}. {file}")
 
-def embed_documents(documents, embed_model, storage, debug: bool = False):
-    # build index of documents if there are no entries in the store
-    if debug: print("\rCreating Index...\t\t\t", end='')
+    print()
+    choice = int(input("Choose a file: ").strip())
 
-    if 'granite' in embed_model:
-        index = VectorStoreIndex.from_documents(
-            documents,
-            embed_model=HuggingFaceEmbedding(model_name=embed_model),
-            cache_folder="models/",
-            show_progress=True,
-            storage_context=storage,
-        )
-    else:
-        index = VectorStoreIndex.from_documents(
-            documents,
-            embed_model=OllamaEmbedding(model_name=embed_model),
-            show_progress=True,
-            storage_context=storage,
-        )
+    document = CustomPDFReader().load_data("data/" + files[choice-1])
 
-    if debug: print("\r"+(" "*50)+"\r", end='')
+    index = reindex_vector_store(document, PG_CONNECTION_URI, PG_DB_NAME, "data_items", "ibm-granite/granite-embedding-278m-multilingual")
 
-    return index
+    multi_hop = MultiHopRAG(index=index, num_passages=10, max_hops=1)
 
-
-def get_index_from_store(vector_store, embed_model):
-    return VectorStoreIndex.from_vector_store(
-        vector_store=vector_store,
-        embed_model=HuggingFaceEmbedding(model_name="ibm-granite/granite-embedding-125m-english"),
-        show_progress=True
+    chatbot = dspy.LM(
+        model="ollama/llama3.1",
+        system_prompt="Strictly follow the given instructions and adhere to the given format",
+        base_url="http://192.168.0.124:11434/",
     )
 
+    dspy.settings.configure(lm=chatbot, rm=MultiHopRAG)
 
-def get_query_engine(index, llm_model, debug: bool = False, **kwargs):
-    # query engine setup
-    if debug: print("\rReadying query engine...\t\t\t", end='')
-
-    query_engine = index.as_query_engine(llm=Ollama(model=llm_model))
-
-    if debug: print("\r"+(" "*50)+"\r", end='')
-    return query_engine
-
-def main():
-    conn = init_pg(PG_CONNECTION_URI)
-    documents = load_documents("data")
-    vector_store, storage = create_vector_store(PG_CONNECTION_URI, PG_DB_NAME, "items")
-
-    print(get_total_nodes(conn, "data_items"))
-    if get_total_nodes(conn, "data_items") >= len(documents):
-        index = get_index_from_store(vector_store, "ibm-granite/granite-embedding-125m-english")
-    else:
-        index = embed_documents(documents, "ibm-granite/granite-embedding-125m-english", storage, debug=True)
-
-    # query_engine = get_query_engine(index, "orca-mini", debug=True)
-    #
-    # while True:
-    #     try:
-    #         prompt = input("\n>> ")
-    #     except (EOFError, KeyboardInterrupt):
-    #         break
-    #
-    #     if prompt == "/bye":
-    #         break
-    #
-    #     response = query_engine.query(prompt.strip())
-    #     print()
-    #     print("\n".join(textwrap.wrap(str(response), width=80)))
-
-    retriever = index.as_retriever()
-    nodes =  retriever.retrieve("What was the gold standard of computers in 1980")
-
-    print(*nodes)
-
-    qe = index.as_query_engine(llm=Ollama(model="mistral", base_url="http://192.168.0.124:11434"))
-    resp = qe.query("What was the gold standard of computers in 1980")
-
-    print(resp)
+    while True:
+        out = multi_hop(input(">> ").strip())
+        print(out.answer)
+        choice = input("History ([Y]/N)? ").strip().lower()
+        if choice == 'n': continue
+        print(chatbot.inspect_history())
+        print()
 
 
 if __name__ == "__main__":
-    main()
-
+    m_main()
