d92a626e816cbd1d29f8ba316310be5590ed2521
Nikhil Solanke <nikhilsolanke5@gmail.com>
2025-01-21 10:05:29 +0530

    give more log info

--- notlogging/notlogger.py
+++ notlogging/notlogger.py
@@ -2,10 +2,11 @@ from typing import Literal, Optional, Union
 
 class NotALogger:
     
-    def __init__(self):
+    def __init__(self, module: str):
         self.enable = True
         self.to_file = False
         self.log_file = None
+        self.module = module
 
     def log(self, message: str, log_type: Union[Literal['info', 'error'], str] = "info"):
         if not self.enable:
@@ -16,36 +17,36 @@ class NotALogger:
             'error': self.error,
         }
 
-        handlers.get(log_type, self._print_custom(log_type))(message)
+        handlers.get(log_type, self._print_custom(log_type))(message, self.module)
 
-    def info(self, message: str, to_file: Optional[bool] = None):
+    def info(self, message: str, to_file: Optional[bool] = None, *args, **kwargs):
         log_to_file = to_file if to_file is not None else self.to_file
         if log_to_file:
             self._log_file(message, "info")
 
-        if self.enable: self._print_info(message)
+        if self.enable: self._print_info(message, self.module)
 
-    def error(self, message: str, to_file: Optional[bool] = None):
+    def error(self, message: str, to_file: Optional[bool] = None, *args, **kwargs):
         log_to_file = to_file if to_file is not None else self.to_file
         if log_to_file:
             self._log_file(message, "info")
 
-        if self.enable: self._print_error(message)
+        if self.enable: self._print_error(message, self.module)
 
     def _log_file(self, message: str, log_type: Union[Literal['info', 'error'], str] = "info"):
         raise NotImplementedError
 
     @staticmethod
-    def _print_error(message: str):
-        print(f"\033[31m[ERR] {__name__}: {message}\033[0m")
+    def _print_error(message: str, from_: str):
+        print(f"\033[31m[ERR] {from_}: {message}\033[0m")
 
     @staticmethod
-    def _print_info(message: str):
-        print(f"\033[34m[INFO] {__name__}: {message}\033[0m")
+    def _print_info(message: str, from_: str):
+        print(f"\033[34m[INFO] {from_}: {message}\033[0m")
 
     @staticmethod
     def _print_custom(log_type: str):
-        def _print_custom(message: str):
-            print(f"\033[31m[{log_type}] {message}\033[0m")
+        def _print_custom(message: str, from_: str):
+            print(f"\033[31m[{log_type}] {from_}: {message}\033[0m")
 
         return _print_custom
