4b19035c7203f8630542a81003ddfa9250cd5fb4
Nikhil Solanke <nikhilsolanke5@gmail.com>
2025-01-22 14:19:48 +0530

    add logging info + lil bit refactoring

--- parsers.py
+++ parsers.py
@@ -86,6 +86,7 @@ class CustomPDFReader(BaseReader):
             self,
             file: Path,
             extra_info: Optional[Dict] = None,
+            use_artifact_pdf: bool = True,
     ) -> List[Document]:
         """Parse file."""
         if not isinstance(file, Path):
@@ -95,9 +96,20 @@ class CustomPDFReader(BaseReader):
 
         docs = []
 
-        logger.log(f"Processing {file.name}", "DEBUG")
+        logger.info(f"Processing {file.name}")
         preprocessor = PDFPreprocessor(file)
-        pages = preprocessor.forward()
+
+        if use_artifact_pdf:
+            logger.info("Extracting images")
+            try:
+                preprocessor.replace_images("out_images/")
+            except Exception as e:
+                logger.error(f"Failed to extract images: {e}")
+                logger.error("Continuing without extracting images...")
+
+        logger.info("Removing similarities...")
+        pages = preprocessor.deduplicate(direction="down")
+        pages = preprocessor.deduplicate(page_lines=pages, direction="up")
 
         text = ""
         for page in pages:
@@ -109,10 +121,3 @@ class CustomPDFReader(BaseReader):
         docs.append(Document(text=text, metadata=metadata))
 
         return docs
-
-
-if __name__ == '__main__':
-    documents = CustomPDFReader().load_data(Path("data/Control Plan - 20. winding CP rev-28.pdf"))
-
-    print(len(documents))
-    print(documents[0].text)
