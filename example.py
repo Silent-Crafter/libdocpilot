056279655aa598950fe4ceede3bf2b62b48bfaee
Nikhil Solanke <nikhilsolanke5@gmail.com>
2026-02-14 17:52:21 +0530

    bump versions

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ from docpilot.utils.llama_utils import get_vector_store_index, load_docs
 from docpilot.notlogging.notlogger import NotALogger
 from docpilot.utils.image_utils import mappings_to_llamaindex_document
 
-from config import config
+from config import Config as config
 
 logger = NotALogger(__name__)
 logger.enabled = True
@@ -34,7 +34,7 @@ def m_main():
         "answer": print,
     }
 
-    docs = load_docs('data', config.get('PG_CONNECTION_URI'), config.get('embed_table'))
+    docs = load_docs('data', config.PG_CONNECTION_URI, config.embed_table)
     mapping = {}
 
     with open("./labels/new.json") as f:
@@ -42,14 +42,14 @@ def m_main():
 
     images = mappings_to_llamaindex_document(mapping, 'out_images')
 
-    index = get_vector_store_index(docs, config.get('PG_CONNECTION_URI'), config.get('embed_table'), config.get('embed_model'))
-    image_index = get_vector_store_index(images, config.get('PG_CONNECTION_URI'), config.get('image_table', "data_images"), config.get('embed_model'))
+    index = get_vector_store_index(docs, config.PG_CONNECTION_URI, config.embed_table, config.embed_model)
+    image_index = get_vector_store_index(images, config.PG_CONNECTION_URI, "data_images", config.embed_model)
 
     multi_hop = MultiHopRAG(index=index, image_index=image_index, num_passages=5)
     chatbot = dspy.LM(
-        model="ollama/"+config.get('ollama_model'),
+        model="ollama/"+config.ollama_model,
         system_prompt="Strictly follow the given instructions and adhere to the given format",
-        base_url=config.get('ollama_url'),
+        base_url=config.ollama_url,
         cache=False,
     )
 
